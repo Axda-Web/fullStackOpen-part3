3948b25a51fedd1f27c6a6848010acf388342ff7
Alyx DARENNE <alyx.darenne@deptagency.com>
2023-03-12 08:28:58 +0100

    part3/ex13: Add getAll/getById from the database feature

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 morgan.token("body", (req) => {
   return JSON.stringify(req.body);
 });
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
-app.use(express.static('build'))
+app.use(express.static("build"));
 
 let persons = [
   {
@@ -49,18 +51,16 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({})
+    .then((people) => {
+      response.json(people);
+    })
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -104,7 +104,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
